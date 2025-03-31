9737102f3a474e79f74e3d31d6c42aaf96f0444c
Madison Kayo <thedarkmoonminrecraft@gmail.com>
2025-03-31 22:12:01 +0200

    Inventory Slot Warning (deleted unused Import)

--- src/main/java/io/github/markassk/fishonmcextras/hud/HudRenderer.java
+++ src/main/java/io/github/markassk/fishonmcextras/hud/HudRenderer.java
@@ -12,7 +12,6 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 
