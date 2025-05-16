e34c2bbaf01e8b3ab9a4b196984b8115cb989faa
DannyPX <47347980+DannyPX@users.noreply.github.com>
2025-05-16 16:05:13 +0200

    Update version

--- src/main/java/io/github/markassk/fishonmcextras/screens/main/MainScreen.java
+++ src/main/java/io/github/markassk/fishonmcextras/screens/main/MainScreen.java
@@ -47,7 +47,7 @@ public class MainScreen extends Screen {
         textList.add(Text.literal("Welcome to FishOnMC-Extras").formatted(Formatting.WHITE));
         textList.add(TabHandler.instance().player);
         textList.add(Text.empty());
-        textList.add(Text.literal("Version 0.2.1-hotfix.1").formatted(Formatting.GRAY));
+        textList.add(Text.literal("Version 0.2.1-hotfix.2").formatted(Formatting.GRAY));
 
         textList.forEach(text -> {
             context.drawText(textRenderer, text, width / 2 - 4 - textRenderer.getWidth(text), height / 2 - (textList.size() * (textRenderer.fontHeight + 1)) / 2 + count.getAndIncrement() * (textRenderer.fontHeight + 1), 0xFFFFFF, true);
