2e7701432c4a760c1821cf41124357b4db0095d0
DannyPX <47347980+DannyPX@users.noreply.github.com>
2025-07-21 00:18:08 +0200

    2.4.2: Fix alternate, fix config text

--- src/main/java/io/github/markassk/fishonmcextras/FOMC/Types/Fish.java
+++ src/main/java/io/github/markassk/fishonmcextras/FOMC/Types/Fish.java
@@ -78,6 +78,7 @@ public class Fish extends FOMCItem {
                     || itemStack.getItem() == Items.WHITE_DYE
                     || itemStack.getItem() == Items.BLACK_DYE
                     || itemStack.getItem() == Items.GOLD_INGOT
+                    || itemStack.getItem() == Items.PRISMARINE_SHARD
             ) {
                 String line = Objects.requireNonNull(itemStack.getComponents().get(DataComponentTypes.LORE)).lines().get(15).getString();
                 return Fish.getFish(itemStack, Defaults.ItemTypes.FISH, line.substring(line.lastIndexOf(" ") + 1));
